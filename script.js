3487cb8bae6dd3631de1fcadfa4cb44d8ff6ccac
budds300 <budds300@gmail.com>
2021-03-14 13:39:06 +0300

    addition of js

---
+++ script.js
@@ -0,0 +1,32 @@
+const addShedButton= document.getElementById("addShed");
+const calculateIncomeButton= document.getElementById("calculateIncomeButton");
+const shedInput=document.getElementById("shedName");
+const litreInput= document.getElementById("litersCapacity");
+const listElProduction= document.getElementById("totalProduction");
+const shed= "Shed";
+let dataObj;
+let dataArr=[];
+let time, income;
+let yearCalender= {
+    "January":31,
+    "February":29,
+    "March":31,
+    "April":30,
+    "May":31,
+    "June":30,
+    "July":31,
+    "August":31,
+    "September":30,
+    "October":31,
+    "November":30,  
+    "Debember":31
+}
+function addGoal(){
+    const getShedInput=shedInput.value;
+    const getLitreInput= litreInput.value;
+    console.log("the input value for shadename is" + getShedInput);
+    console.log("the input value for liter capacity is" +  getLitreInput);
+    const literItemEl= document.createElement("li");
+    
+
+}
