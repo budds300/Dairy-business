09fe432111b21f69e385ce98de7fb67549545113
budds300 <budds300@gmail.com>
2021-03-14 18:43:38 +0300

    fixed images

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const addShedButton= document.getElementById("addShed");
-const calculateIncomeButton= document.getElementById("calculateIncomeButton");
-const shedInput=document.getElementById("shedName");
-const litreInput= document.getElementById("litersCapacity");
-const listElProduction= document.getElementById("totalProduction");
-const shed= "Shed";
+const addShedButton = document.getElementById("addShedButton");
+const calculateIncomeButton = document.getElementById("calculateIncomeButton");
+const shedInput = document.getElementById("shedName");
+const litreInput = document.getElementById("litresCapacity");
+const listElProduction = document.getElementById("totalProduction");
+const listElIncomeList = document.getElementById("displayIncome");
+const shed = "Shed";
 let dataObj;
-let dataArr=[];
+let dataArr = [];
 let time, income;
-let yearCalender= {
+let yearCalender = {
     "January":31,
     "February":29,
     "March":31,
@@ -21,31 +22,34 @@ let yearCalender= {
     "November":30,  
     "Debember":31
 }
-function addGoal(){
-    const getShedInput=shedInput.value;
-    const getLitreInput= litreInput.value;
-    console.log("the input value for shadename is" + getShedInput);
-    console.log("the input value for liter capacity is" +  getLitreInput);
-    const listItemEl= document.createElement("li");
-    listItemEl.textContent= "shed" + " " + getShedInput + ": " + getLitreInput;
-    listElProduction.appendChild(listItemEl);
-    shedInput.value="";
-    litreInput.value="";
-    data= {shed: getShedInput, capacity:getShedInput};
-    dataArr.push(data);
-    console.log(JSON.stringify(dataArr));
+
+function addGoal() {
+  const getShedInput = shedInput.value;
+  const getLitreInput = litreInput.value;
+  console.log("the input value for shedname is " + getShedInput);
+  console.log("the input value for litre capacity is " + getLitreInput);
+  const listItemEl = document.createElement("li");
+  listItemEl.textContent = "shed" + " " + getShedInput + ": " + getLitreInput;
+  listElProduction.appendChild(listItemEl);
+  shedInput.value = "";
+  litreInput.value = "";
+  data = { shed: getShedInput, capacity: getLitreInput };
+  dataArr.push(data);
+  console.log(JSON.stringify(dataArr));
 }
-function calculateIncome(){
-    let objvalue,
-    totalLiters=0;
-    const sellingPrice= document.getElementById("sellingPrice").value;
-    const yearSelected= document.querySelector('input[name="year]:checked').value
-    let totalIncomeWeekly, totalIncomeYearly, totalIncomeLeapYear;
+
+function calculateIncome() {
+  let objvalue,
+    totalLitres = 0;
+  const sellingPrice = document.getElementById("sellingPrice").value;
+  const yearSelected = document.querySelector('input[name="year"]:checked')
+    .value;
+  let totalIncomeWeekly, totalIncomeYearly, totalIncomeLeapYear;
   let monthListItemEl = document.createElement("li");
+
   if (sellingPrice === "") {
     alert("Please enter selling price to proceed");
-  }
-  else {
+  } else {
     for (let index = 0; index < dataArr.length; index++) {
       objvalue = dataArr[index].capacity;
       totalLitres += parseInt(objvalue);
@@ -53,28 +57,29 @@ function calculateIncome(){
     }
     console.log(sellingPrice * totalLitres);
     if (yearSelected == "regular") {
-        totalIncomeWeekly = sellingPrice * totalLitres * 7;
-        totalIncomeYearly = sellingPrice * totalLitres * 365;
-        document.getElementById("dailyIncome").innerHTML = sellingPrice * totalLitres;
-        document.getElementById("WeeklyIncome").innerHTML = totalIncomeWeekly;
-        document.getElementById("yearlyIncome").innerHTML = totalIncomeYearly;
-        }
-        else if (yearSelected == "leap") {
-            totalIncomeWeekly = sellingPrice * totalLitres * 7;
-            totalIncomeYearly = sellingPrice * totalLitres * 366;
-            document.getElementById("dailyIncome").innerHTML = sellingPrice * totalLitres;
-            document.getElementById("WeeklyIncome").innerHTML = totalIncomeWeekly;
+      totalIncomeWeekly = sellingPrice * totalLitres * 7;
+      totalIncomeYearly = sellingPrice * totalLitres * 365;
+      document.getElementById("dailyIncome").innerHTML = sellingPrice * totalLitres;
+      document.getElementById("WeeklyIncome").innerHTML = totalIncomeWeekly;
+      document.getElementById("yearlyIncome").innerHTML = totalIncomeYearly;
+    } else if (yearSelected == "leap") {
+      totalIncomeWeekly = sellingPrice * totalLitres * 7;
+      totalIncomeYearly = sellingPrice * totalLitres * 366;
+      document.getElementById("dailyIncome").innerHTML = sellingPrice * totalLitres;
+      document.getElementById("WeeklyIncome").innerHTML = totalIncomeWeekly;
       document.getElementById("yearlyIncome").innerHTML = totalIncomeYearly;
       for (const property in yearCalender) {
         // listElIncomeList
-        console.log(`Month of  ${property} :  ${yearCalender[property] * sellingPrice}`)
+        console.log("Month of " + `${property}` + ":  " + (`${yearCalender[property]}`) *sellingPrice)
+        
       }
     }
+  }
 }
-}
-calculateIncomeButton.addEventListener("click", calculateIncome);
+// calculateIncome(90,89)
+// document.getElementById("sellingPrice").addEventListener("input", calculateIncome);
+calculateIncomeButton.addEventListener("click", calculateIncome); //incase you need to use the button to call the tocalculate total rather than the input evemt
 document.querySelectorAll("input[name='year']").forEach((input) => {
-    input.addEventListener("change", calculateIncome);
-  });
-  addShedButton.addEventListener("click", addGoal);
-
+  input.addEventListener("change", calculateIncome);
+});
+addShedButton.addEventListener("click", addGoal);
